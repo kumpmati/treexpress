e702373320fa83089be360c0918fe3eec925518a
kumpmati <kumpulainen.matias@gmail.com>
2021-06-02 12:42:43 +0300

    simple test app

--- src/test.tsx
+++ src/test.tsx
@@ -4,34 +4,17 @@ import GET from './lib/components/GET'
 import Router from './lib/components/Router'
 import { FC } from './lib/jsxFactory'
 
-const Api: FC<{ path: string }> = ({ path, children }) => {
+const Api: FC<{ path: string }> = ({ path }) => {
   return (
     <Router path={path}>
-      <GET fn={(_, res) => res.json({ hello: 'from API' })} />
-      {children}
-    </Router>
-  )
-}
-
-const B: FC<{ path: string }> = ({ path, children }) => {
-  return (
-    <Router path={path}>
-      <GET fn={(_, res) => res.json({ hello: 'from B' })} />
-      {children}
+      <GET fn={(_, res) => res.send('hello from API').end()} />
     </Router>
   )
 }
 
 start(
   <Server port={8000}>
-    <GET fn={(_, res) => res.json({ hello: 'from root' })} />
-    <Api path="/api">
-      <Router path="/inner">
-        <GET fn={(_, res) => res.json({ hello: 'from API/inner' })} />
-      </Router>
-      <B path="/b">
-        <GET path="/haha" fn={(_, res) => res.json({ hello: 'from B/haha' })} />
-      </B>
-    </Api>
+    <GET fn={(_, res) => res.send('hello from root').end()} />
+    <Api path="/api" />
   </Server>,
 )
